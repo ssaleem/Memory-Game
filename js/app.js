81a8132eb1a9d5c73191451708f073da8265bc20
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-05-17 15:26:53 -0400

    Organize code by removing redundant lines

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-// First fill the array with the values in order.
+// Deck card values
 let cities = ['Chicago', 'Los Angeles', 'Miami', 'Las Vegas', 'Atlanta', 'Seattle', 'New York', 'Houstan', 'Chicago', 'Los Angeles', 'Miami', 'Las Vegas', 'Atlanta', 'Seattle', 'New York', 'Houstan' ];
 let languages = ['Arabic', 'Spanish', 'Portuguese', 'English', 'German', 'Russian', 'Hindi', 'Chinese', 'Arabic', 'Spanish', 'Portuguese', 'English', 'German', 'Russian', 'Hindi', 'Chinese' ];
 let brandIcons = ['fa-amazon', 'fa-apple', 'fa-android', 'fa-btc', 'fa-chrome', 'fa-dropbox', 'fa-facebook-official', 'fa-github','fa-amazon',
 'fa-apple', 'fa-android', 'fa-btc', 'fa-chrome', 'fa-dropbox', 'fa-facebook-official', 'fa-github']
-// Go through the array and exchange each element with the randomly chosen element in the range
-// from itself to the end.
 const itemsCount = 16;
-let openCardCount = 0;
 let openCards = [];
-let matchedCards = [];
+// let timer;
+
+
+// Game Panel Controls
+const moves = document.querySelector('.moves');
 let movesCount = 0;
-let timeInSeconds = 0;
+const stars = document.getElementsByClassName('fa-star');
 let starCount = 3;
+const reset = document.querySelector('.reset');
+const timeDisplay = document.querySelector('.time-display');
+let timeInSeconds = 0;
+
+// Main Deck
+const deck = document.querySelector('.deck');
+const cardTexts = document.getElementsByClassName('card-text');
+
+// Live collection of open and matched cards
+const matched = document.getElementsByClassName('matched');
+const open = document.getElementsByClassName('open');
+
+// Game completed Modal
+const modal = document.querySelector('.win-modal');
+const modalTime = document.querySelector('.modal-time');
+const modalStars = document.querySelector('.modal-stars');
+const modalYes = document.querySelector('.modal-yes');
+const modalNo = document.querySelector('.modal-no');
+
+
 function shuffle(items){
 	for(let index = 0; index < itemsCount; index++){
 		let randomIndex = Math.floor(Math.random() * (itemsCount - index)) + index;
@@ -22,30 +43,34 @@ function shuffle(items){
 		}
 	}
 }
-// Game Panel Controls
-const moves = document.querySelector('.moves');
-const reset = document.querySelector('.reset');
-const timeDisplay = document.querySelector('.time-display');
-// Main Deck
-const deck = document.querySelector('.deck');
-const cardTexts = document.getElementsByClassName('card-text');
-const stars = document.getElementsByClassName('fa-star');
-// Win Modal
-const modal = document.querySelector('.win-modal');
-const modalYes = document.querySelector('.modal-yes');
-const modalNo = document.querySelector('.modal-no');
-const modalTime = document.querySelector('.modal-time');
-const modalStars = document.querySelector('.modal-star');
 
+function loadDeck(){
+	shuffle(cities);
+	let item = 0;
+	for(const text of cardTexts){
+		text.textContent = cities[item];
+		item++;
+	}
+}
 
 function resetGame(){
 	//reset game here
-	// use query selector to pick elements with open/matched classes and remove them
-	//call loadDeck()
+	// 1. Clear last deck
+	// use live collection of elements with open/matched classes and remove those classes
+	while(open[0]) {
+		open[0].classList.remove('open','matched');
+	}
+	// clear the openCards array in case game was reset in the middle of play
+	openCards = [];
+
+	// 2. Create new deck
+	loadDeck();
+
+	// 3. Reset Game Panel Controls
 	//reset stars
 	starCount = 3;
 	for(const star of stars){
-		star.classList.remove('hide-star');
+		star.classList.remove('fa-star-o');
 	}
 	// set moves to 0
 	movesCount = 0;
@@ -56,34 +81,39 @@ function resetGame(){
 	timeDisplay.textContent = timeInSeconds;
 }
 
-function loadDeck(){
-	shuffle(cities);
-	let item = 0;
-	for(const text of cardTexts){
-		text.textContent = cities[item];
-		item++;
-	}
-}
+
 function displaySeconds(){
 	timeInSeconds++;
 	timeDisplay.textContent = timeInSeconds;
 }
 
 function setStars(){
-	console.log('I am in setStars')
-	if(movesCount === 20){
-		stars[2].classList.add('hide-star');
-		starCount--;
-	}
-	else if(movesCount === 14) {
-		stars[1].classList.add('hide-star');
+	if(movesCount === 15) {
+		stars[1].classList.add('fa-star-o');
 		starCount--;
 	}
 	else if(movesCount === 9) {
-		stars[0].classList.add('hide-star');
+		stars[0].classList.add('fa-star-o');
 		starCount--;
 	}
 }
+
+function gameCompleted(){
+	clearInterval(timer);
+	modal.classList.add('show');
+	modalTime.textContent = `Game Completion Time: ${timeInSeconds} seconds`;
+	// xmodalStars =
+	let htmlString = "Rating: ";
+	for(let i = 1; i <= starCount; i++){
+		htmlString += '<i class="fa fa-star"></i>';
+
+	}
+	modalStars.innerHTML = htmlString;
+
+}
+
+// Event listeners
+
 document.addEventListener('DOMContentLoaded', function () {
 	loadDeck();
 });
@@ -92,7 +122,6 @@ deck.addEventListener('click', function(event){
 	event.preventDefault();
 	// start timer if it already is not running
 	if(timeInSeconds === 0){
-		// modal.classList.add('show');
 		timer = setInterval(displaySeconds, 1000);
 	}
 	if(!event.target.classList.contains('matched')) {
@@ -109,10 +138,9 @@ deck.addEventListener('click', function(event){
 				openCards = [];
 			}
 			openCards.push(event.target);
-			console.log(openCards);
-			console.log(openCards.length);
 			event.target.classList.add('open');
 			event.target.firstElementChild.classList.add('mirror');
+			console.log(openCards.length);
 			//matched
 			if(openCards.length === 2 && (openCards[0].textContent === openCards[1].textContent)){
 				movesCount++;
@@ -120,16 +148,13 @@ deck.addEventListener('click', function(event){
 					setStars();
 				}
 				moves.textContent = movesCount;
-				matchedCards.push(openCards[0].textContent);
 				for(const card of openCards){
 					card.classList.add('matched');
 				}
-				if(matchedCards.length == 8){
+				//check for winning condition
+				if(matched.length == itemsCount){
+					gameCompleted();
 					console.log('game over');
-					modal.classList.add('show');
-					modalTime.textContent = `Game Completion Time: ${timeInSeconds}`;
-
-					//stop timer
 				}
 				openCards = [];
 			}
@@ -141,19 +166,19 @@ reset.addEventListener('click', function(){
 	resetGame();
 });
 
-// modal functionality
-
+// Modal related event listeners
 // When the user clicks on No <button>, close the modal
 modalNo.onclick = function() {
 	modal.classList.remove('show');
 }
 
+// When the user clicks on Yes <button>, close the modal, restart the game
 modalYes.onclick = function() {
 	modal.classList.remove('show');
 	resetGame();
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close the modal
 window.onclick = function(event) {
 	if (event.target == modal) {
 		modal.classList.remove('show');
